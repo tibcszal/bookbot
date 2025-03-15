3e4106ac429e3a677f013e1a2908e868092a25ea
tcz <tiborcsendes.z@gmail.com>
2025-03-15 21:19:29 +0100

    char count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count
+from stats import *
 
 def get_book_text(fpath):
     with open(fpath, "r") as f:
@@ -9,7 +9,9 @@ def main():
     path = "books/frankenstein.txt"
     file_contents = get_book_text(path)
     num_words = get_word_count(file_contents)
+    num_chars = get_char_count(file_contents)
     print(f"{num_words} words found in the document")
+    print(num_chars)
     
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 
 def get_word_count(file_contents):
     return len(file_contents.split())
+
+
+def get_char_count(file_contents):
+    result_dict = {}
+    for char in file_contents:
+        lower_case_char = char.lower()
+        if lower_case_char not in result_dict.keys():
+            result_dict[lower_case_char] = 1
+        else:
+            result_dict[lower_case_char] += 1
+    return result_dict
